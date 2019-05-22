2232c66b71bba4f4d9d3c1504d23de73b7c5458f
Troy Liu <troyliu0105@outlook.com>
2019-05-22 20:07:21 +0800

    UPDATE: add shuffle to create_nsfw_dataset.cpp

--- tools/generic/create_nsfw_dataset.cpp
+++ tools/generic/create_nsfw_dataset.cpp
@@ -20,6 +20,7 @@
 #include "caffe/util/format.hpp"
 #include "caffe/util/data.hpp"
 #include "caffe/util/io.hpp"
+#include "caffe/util/rng.hpp"
 
 using caffe::DatumPlus;
 using caffe::compress;
@@ -29,15 +30,22 @@ using std::string;
 namespace db = caffe::db;
 namespace bfs = boost::filesystem;
 
-void create_dataset(const string &root_folder, const string &output_folder, const string &db_type) {
+void create_dataset(const string &root_folder,
+                    const string &output_folder,
+                    const string &db_type,
+                    const string &phase) {
   scoped_ptr<db::DB> train_db(db::GetDB(db_type));
-  train_db->Open(output_folder + "/nsfw_test_" + db_type, db::NEW);
+  train_db->Open(output_folder + "/nsfw_" + phase + "_" + db_type, db::NEW);
   scoped_ptr<db::Transaction> txn(train_db->NewTransaction());
   bfs::directory_iterator root(root_folder);
   float label = 0;
   int fileid = 0;
+  int resized_height = 224;
+  int resized_width = 224;
+  std::vector<std::pair<string, float>> imgs_labels;
   string data;
   DatumPlus datum;
+  // get all files
   for (const bfs::directory_entry &dir: root) {
     if (bfs::is_directory(dir)) {
       LOG(INFO) << "Entering "
@@ -46,70 +54,64 @@ void create_dataset(const string &root_folder, const string &output_folder, cons
         auto ext = boost::algorithm::to_lower_copy(bfs::extension(file.path()));
         if (bfs::is_regular_file(file.status())
             && (ext == ".jpg" || ext == ".png")) {
-          datum.clear_height();
-          datum.clear_width();
-          datum.clear_channels();
-          datum.clear_data();
-          datum.clear_label();
-
-//          bfs::ifstream ifs(file.path(), std::ios::in | std::ios::binary | std::ios::ate);
-//          bfs::ifstream::pos_type fileSize = ifs.tellg();
-//          ifs.seekg(0, std::ios::beg);
-//          std::vector<char> bytes(fileSize);
-//          ifs.read(bytes.data(), fileSize);
-//          string data(bytes.data(), fileSize);
-//
-          cv::Mat pic = cv::imread(file.path().string());
-          if (pic.empty())
-            continue;
-          cv::Mat out;
-          if (pic.channels() == 1) {
-            cv::cvtColor(pic, out, cv::COLOR_GRAY2BGR);
-            cv::imwrite(file.path().string(), out);
-          } else if (pic.channels() == 4) {
-            cv::cvtColor(pic, out, cv::COLOR_BGRA2BGR);
-            cv::imwrite(file.path().string(), out);
-          }
-//          datum.set_height(pic.rows);
-//          datum.set_width(pic.cols);
-//          datum.set_channels(pic.channels());
-//
-//          uint32_t size = pic.channels() * pic.rows * pic.cols;
-//          data.resize(size);
-//          std::memcpy(&data[0], pic.data, size * sizeof(*pic.data));
-//
-//          datum.set_data(compress(data));
-//          datum.add_label(label);
-
-          caffe::ReadImageToDatum(file.path().string(), {label}, string("jpg"), &datum, false);
-          string value;
-          CHECK(datum.SerializeToString(&value));
-
-          txn->Put(caffe::format_int(fileid, 8), value);
-          if (fileid % 500 == 0) {
-            LOG(INFO) << "Commit...";
-            txn->Commit();
-          }
-          fileid++;
+          imgs_labels.emplace_back(std::make_pair(file.path().string(), label));
         }
       }
       label++;
     }
   }
+  LOG(INFO) << "Get " << imgs_labels.size() << "images in total";
+
+  // shuffle
+  caffe::shuffle(imgs_labels.begin(), imgs_labels.end());
+
+  for (auto &p : imgs_labels) {
+    auto path = p.first;
+    auto l = p.second;
+    datum.clear_height();
+    datum.clear_width();
+    datum.clear_channels();
+    datum.clear_data();
+    datum.clear_label();
+
+    datum.set_height(resized_height);
+    datum.set_width(resized_width);
+    datum.set_channels(3);
+
+    bool success = caffe::ReadImageToDatum(path,
+                                           {l},
+                                           resized_height,
+                                           resized_width,
+                                           true,
+                                           string("jpg"),
+                                           &datum,
+                                           false);
+    if (!success)
+      continue;
+    string value;
+    CHECK(datum.SerializeToString(&value));
+
+    txn->Put(caffe::format_int(fileid, 8), value);
+    ++fileid;
+    if (fileid % 5000 == 0) {
+      txn->Commit();
+    }
+  }
+
   txn->Commit();
   train_db->Close();
 }
 
 int main(int argc, char **argv) {
   FLAGS_alsologtostderr = true;
-  if (argc != 4) {
+  if (argc != 5) {
     std::cout << "Usage:\n"
                  "    create_nsfw_dataset root_folder output_folder db_type\n"
                  "Where the input folder should contain the binary batch files.\n"
                  "You should gunzip them after downloading.\n" << std::endl;
   } else {
     google::InitGoogleLogging(argv[0]);
-    create_dataset(string(argv[1]), string(argv[2]), string(argv[3]));
+    create_dataset(string(argv[1]), string(argv[2]), string(argv[3]), string(argv[4]));
   }
   return 0;
 }
